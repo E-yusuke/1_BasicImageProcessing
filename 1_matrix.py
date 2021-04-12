b65f1ac4a2d9acacec4e780b90b7ecbc6029fddc
yusuke <yusuke.nagata@human.ait.kyushu-u.ac.jp>
2021-04-12 14:51:35 +0900

    first

---
+++ 1_matrix.py
@@ -0,0 +1,12 @@
+# numpyを使った行列の四則演算
+import numpy as np
+
+a = np.array([[12, 2], [3, 45]])
+b = np.array([[34, 5], [67, 8]])
+print('a', a)
+print('b', b)
+print('a+b\n', a+b)
+print('a-b\n', a-b)
+print('a*b\n', a*b)
+print('a/b\n', a/b)
+print('a%b\n', a % b)
